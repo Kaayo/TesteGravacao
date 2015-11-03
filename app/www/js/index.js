6173c2cbe78a345beeba79f7b88e6b7330528dae
Kaayo <caio_figurski@hotmail.com>
2015-11-03 14:26:47 -0200

    Teste Gravação 2.0

---
+++ app/www/js/index.js
@@ -0,0 +1,74 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+/*var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    }
+}; */
+
+
+document.addEventListener("deviceready", init, false);
+function init() {
+    
+    
+    document.querySelector("#takeVideo").addEventListener("touchend", function() {
+        console.log("Take video");
+        navigator.device.capture.captureVideo(captureSuccess, captureError, {limit: 1});
+    }, false);
+    
+}
+
+function captureError(e) {
+    console.log("capture error: "+JSON.stringify(e));
+}
+
+function captureSuccess(s) {
+    console.log("Success");
+    console.dir(s[0]);
+    var v = "<video controls='controls'>";
+    v += "<source src='" + s[0].fullPath + "' type='video/mp4'>";
+    v += "</video>";
+    document.querySelector("#videoArea").innerHTML = v;
+}
